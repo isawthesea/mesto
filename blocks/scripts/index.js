51da52af0faa0bf112e39cbab7a605e72ef155f8
isawthesea <90389776+isawthesea@users.noreply.github.com>
2021-10-29 17:46:37 +0300

    fix: popup is done

--- blocks/scripts/index.js
+++ blocks/scripts/index.js
@@ -3,9 +3,9 @@ const closeButton = popupItem.querySelector('.popup__close-button');
 const openPopup = document.querySelector('.profile__edit-button');
 
 openPopup.addEventListener('click', () => {
-  popupItem.classList.remove('hidden');
+  popupItem.classList.add('popup_opened');
 })
 
 closeButton.addEventListener('click', () => {
-  popupItem.classList.add('hidden');
+  popupItem.classList.remove('popup_opened');
 })
