bbf021a38b70377db3acb58a757d2a3e8f39fd00
isawthesea <90389776+isawthesea@users.noreply.github.com>
2021-12-01 22:45:41 +0300

    fix: 1st review

--- scripts/validate.js
+++ scripts/validate.js
@@ -23,26 +23,39 @@ const checkInputValidity = (formElement, inputElement, config) => {
   }
 }
 
-const toggleButtonState = (formElement, buttonElement, config) => {
+const toggleButtonState = (formElement, config) => {
   // Проверяем валидность формы
   const isFormValid = formElement.checkValidity();
   // Если форма невалидна, то присваиваем свойству disabled кнопки значение true
+  const buttonElement = formElement.querySelector(config.buttonSelector);
   buttonElement.classList.toggle(config.inactiveButtonClass, !isFormValid) //false
   buttonElement.disabled = !isFormValid; //true
 }
 
 const enableValidation = (config) => {
   // находим все поля ввода
-  const formElement = document.querySelector(config.formSelector);
-  formElement.addEventListener('submit', (evt) => {evt.preventDefault()});
-  const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
-  // находим кнопку отправки формы
-  const buttonElement = formElement.querySelector(config.buttonSelector);
-  toggleButtonState(formElement, buttonElement, config);
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', () => {
-          checkInputValidity(formElement, inputElement, config);
-          toggleButtonState(formElement, buttonElement, config);
-      });
+  const formElements = document.querySelectorAll(config.formSelector);
+  formElements.forEach(function(formElement) {
+    formElement.addEventListener('submit', (evt) => {evt.preventDefault()});
+    const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+    // находим кнопку отправки формы
+    toggleButtonState(formElement, config);
+    inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', () => {
+            checkInputValidity(formElement, inputElement, config);
+            toggleButtonState(formElement, config);
+        });
+    });
   });
 }
+
+const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  buttonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputElementClass: 'popup__input_type-error',
+  errorElementClass: 'popup__input-error'
+}
+
+enableValidation(validationConfig);
